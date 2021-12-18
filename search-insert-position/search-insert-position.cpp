ea0442907e41cea99eafd03a556c41c11b3c8f2e
Haoli Yin <haoli.yin@vanderbilt.edu>
2021-12-18 14:34:41 -0600

    Time: 4 ms (80.22%), Space: 9.6 MB (71.78%) - LeetHub

--- search-insert-position/search-insert-position.cpp
+++ search-insert-position/search-insert-position.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        if(nums.empty() || nums.size() == 0) return 0;
-		
-		int n = nums.size();
-		int left = 0;
-		int right = n - 1;
-		while(left < right){
-			int mid = left + (right - left)/2;
-			
-			if(nums[mid] == target) return mid;
-			else if(nums[mid] > target) right = mid; // right could be the result
-			else left = mid + 1; // mid + 1 could be the result
-		}
-		
-		// 1 element left at the end
-		// post-processing
-		return nums[left] < target ? left + 1: left;
+        if(nums.empty() || nums.size()==0) return 0;
+        int a=0, b=nums.size()-1, m; 
+        while(a < b){
+            m = a + (b-a)/2;
+            if(nums[m] == target) return m; 
+            if(nums[m] > target) b = m; 
+            else a = m+1;
+        }
+        return  nums[a] < target ? a + 1: a;
     }
 };
