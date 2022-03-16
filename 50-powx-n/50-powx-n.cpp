7b2cebf912ede71329c0a3a3965c2406cb2bd74f
Haoli Yin <haoli.yin@vanderbilt.edu>
2022-03-15 21:45:29 -0500

    Time: 0 ms (100.00%), Space: 6 MB (18.66%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == 0) return 1;
+        else if (n%2 == 0) return pow(x*x, n/2);
+        else return x*pow(x, n-1);
+    }
+};
